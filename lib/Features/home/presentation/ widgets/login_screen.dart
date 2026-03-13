d399d03ea8035d977f8bbeb6e2fe7fc5cf747bfd
MariamSaad16 <mariam1khalifa6@gmail.com>
2026-03-13 13:30:30 +0200

    WIdgets_Updates

---
+++ lib/Features/home/presentation/ widgets/login_screen.dart
@@ -0,0 +1,383 @@
+import 'package:bookle_app/core/utils/assets.dart';
+import 'package:bookle_app/costants.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(65),
+        child: Container(
+          padding: EdgeInsets.only(top: 30),
+          child: AppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            leading: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.arrow_back, size: 30),
+            ),
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          padding: EdgeInsets.symmetric(horizontal: 25),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _loginHeader(),
+              SizedBox(height: 20),
+              _TextForm(),
+              SizedBox(height: 3),
+              _TextForm2(),
+              SizedBox(height: 10),
+                    
+              Align(
+                alignment: Alignment.centerLeft,
+                child: TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Forgot Password?",
+                    style: GoogleFonts.roboto(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 14,
+                      height: 1.4,
+                      color: kPrimaryColor,
+                    ),
+                  ),
+                ),
+              ),
+                    
+              SizedBox(height: 20),
+              Login(),
+              SizedBox(height: 20),
+              Line(),
+              SizedBox(height: 20),
+              Line2(),
+              SizedBox(height: 15),
+              Final(),
+              SizedBox(height: 4,),
+              Final2(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _loginHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SizedBox(height: 30),
+        Text(
+          "Welcome Back 👋",
+          style: GoogleFonts.openSans(
+            fontWeight: FontWeight.bold,
+            fontSize: 24,
+            height: 1.35,
+            color: Colors.grey[900],
+          ),
+        ),
+        SizedBox(height: 10),
+        Text(
+          "Sign to your account",
+          style: GoogleFonts.roboto(
+            fontWeight: FontWeight.w400,
+            fontSize: 16,
+            height: 1.5,
+            color: Colors.grey[500],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _TextForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [_label("Email"), _input1()],
+    );
+  }
+
+  Widget _label(String text) => Padding(
+    padding: const EdgeInsets.only(bottom: 10, left: 4),
+
+    child: Text(
+      text,
+      style: GoogleFonts.roboto(
+        fontSize: 20,
+        fontWeight: FontWeight.w500,
+        color: kGrey900,
+        height: 1.4,
+      ),
+    ),
+  );
+
+  Widget _input1() {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(9),
+        border: Border.all(color: Color(0xffFAFAFA), width: 1.5),
+        boxShadow: [
+          BoxShadow(
+            blurRadius: 10,
+            color: Colors.grey.withValues(alpha: .03),
+            offset: const Offset(0, 4),
+          ),
+        ],
+      ),
+      child: TextField(
+        style: TextStyle(color: Colors.black, fontWeight: FontWeight.w400),
+        decoration: InputDecoration(
+          hintText: "Your email",
+          hintStyle: GoogleFonts.roboto(
+            fontWeight: FontWeight.w400,
+            fontSize: 16,
+            height: 1.5,
+            color: Color(0xFFE0E0E0),
+          ),
+          border: InputBorder.none,
+          contentPadding: const EdgeInsets.symmetric(vertical: 18),
+        ),
+      ),
+    );
+  }
+}
+
+class _TextForm2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [_labe2("Password"), _input2()],
+    );
+  }
+
+  Widget _labe2(String text) => Padding(
+    padding: const EdgeInsets.only(bottom: 10, left: 4),
+
+    child: Text(
+      text,
+      style: GoogleFonts.roboto(
+        fontSize: 20,
+        fontWeight: FontWeight.w500,
+        color: kGrey900,
+        height: 1.4,
+      ),
+    ),
+  );
+
+  Widget _input2() {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(9),
+        border: Border.all(color: Color(0xffFAFAFA), width: 1.5),
+        boxShadow: [
+          BoxShadow(
+            blurRadius: 10,
+            color: Colors.grey.withValues(alpha: .03),
+            offset: const Offset(0, 4),
+          ),
+        ],
+      ),
+      child: TextField(
+        obscureText: true,
+        style: TextStyle(color: Colors.black, fontWeight: FontWeight.w400),
+        decoration: InputDecoration(
+          hintText: "Your password",
+          suffixIcon: IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.visibility_off),
+          ),
+          hintStyle: GoogleFonts.roboto(
+            fontWeight: FontWeight.w400,
+            fontSize: 16,
+            height: 1.5,
+            color: Color(0xFFE0E0E0),
+          ),
+          border: InputBorder.none,
+          contentPadding: const EdgeInsets.symmetric(vertical: 18),
+        ),
+      ),
+    );
+  }
+}
+
+class Login extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Color(0xff54408C),
+        minimumSize: Size(double.infinity, 50),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
+      ),
+      child: Text(
+        "Login",
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.w700,
+          fontSize: 16,
+        ),
+      ),
+    );
+  }
+}
+
+class Line extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          "Don’t have an account?",
+          style: GoogleFonts.roboto(
+            fontWeight: FontWeight.w500,
+            fontSize: 16,
+            height: 1.5,
+            color: Colors.grey[500],
+          ),
+        ),
+        TextButton(
+          style: TextButton.styleFrom(
+            padding: EdgeInsets.zero, // removes space
+            minimumSize: Size.zero,
+            tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+          ),
+          onPressed: () {},
+          child: Text(
+            " Sign Up",
+            style: GoogleFonts.roboto(
+              fontWeight: FontWeight.w500,
+              fontSize: 16,
+              height: 1.5,
+              color: kPrimaryColor,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class Line2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(child: Divider()),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: Text(
+            "Or with",
+            style: GoogleFonts.roboto(
+              fontWeight: FontWeight.w400,
+              height: 1.4,
+              fontSize: 14,
+              color: Colors.grey[500],
+            ),
+          ),
+        ),
+        Expanded(child: Divider()),
+      ],
+    );
+  }
+}
+
+class Final extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: 50,
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(25),
+            side: BorderSide(color: Colors.grey.shade300),
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+           Image.asset(AssetData.google ,width: 15,),
+            SizedBox(width: 10),
+            Text(
+              "Sign in with Google",
+              style: GoogleFonts.roboto(
+                fontWeight: FontWeight.w400,
+                fontSize: 14,
+                height: 1.5,
+                color: Colors.grey.shade900,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Final2 extends StatelessWidget {
+  const Final2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: 50,
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(25),
+            side: BorderSide(color: Colors.grey.shade300),
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.apple, size: 30, color: Colors.black),
+            SizedBox(width: 10),
+            Text(
+              "Sign in with Apple",
+              style: GoogleFonts.roboto(
+                fontWeight: FontWeight.w400,
+                fontSize: 14,
+                height: 1.5,
+                color: Colors.grey.shade900,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/Features/home/presentation/ widgets/on_boarding_2.dart
@@ -0,0 +1,160 @@
+import 'package:bookle_app/core/utils/assets.dart';
+import 'package:bookle_app/costants.dart';
+import 'package:bookle_app/features/presentation/views/signUp.dart';
+import 'package:bookle_app/features/presentation/views/widgets/on_boarding_3.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:get/get.dart';
+
+
+class OnBoarding2 extends StatelessWidget {
+  const OnBoarding2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [        
+          SizedBox(height: 60,),
+          SkipButton(),
+          SizedBox(height: 13,),
+          Images(),
+          SizedBox(height: 14,),
+          Text1(),
+          SizedBox(height: 38,),
+          TwoButtons(),
+          
+        ],
+      ),
+    );
+  }
+}
+
+class SkipButton extends StatelessWidget {
+  const SkipButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(left: 24),
+      child: TextButton(onPressed: (){},
+       child: Text("Skip",style: GoogleFonts.roboto(
+                fontWeight: FontWeight.w500,fontSize: 18,
+                height: 1.4,color: kPrimaryColor
+               ),),)
+    );
+  }
+}
+
+class Images extends StatelessWidget {
+  const Images({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return   Padding(
+      padding: const EdgeInsets.only(left: 27,right: 28),
+      child: Image.asset("images/Frame_2.png"),
+    );
+  }
+}
+
+class Text1 extends StatelessWidget {
+  const Text1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+       Container(
+            padding: const EdgeInsets.only(left: 66, right: 66),
+
+            child: Text(
+               "Your Bookish Soulmate\nAwaits",
+              style: GoogleFonts.openSans(
+                fontWeight: FontWeight.w700,
+                fontSize: 26,
+                letterSpacing: -1.2,
+                color: Colors.grey[900],
+                height: 1.35,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          SizedBox(height: 15),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 40),
+            child: Text(
+  "Let us be your guide to the perfect read.\n Discover books tailored to your tastes\n for a truly rewarding experience.",              style: GoogleFonts.roboto(
+                fontSize: 18,
+                fontWeight: FontWeight.w400,
+                letterSpacing: 0,
+                color: Colors.grey[500],
+                height: 1.6,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          SizedBox(height: 45),
+          Image.asset(AssetData.second),
+    ],);
+  }
+}
+
+
+class TwoButtons extends StatelessWidget {
+  const TwoButtons({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+
+      Container(
+            padding: const EdgeInsets.only(left: 24, right: 24),
+            child: ElevatedButton(
+              onPressed: () {
+                Get.to(OnBoarding3());
+              },
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(vertical: 15),
+                backgroundColor: kPrimaryColor,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+              child: Center(
+                child: Text("Get started", style: GoogleFonts.openSans(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w700,
+                  fontSize: 18,
+                  height: 1.5,
+                  letterSpacing: .3)),
+              ),
+            ),
+          ),
+          SizedBox(height: 8),
+          Container(
+            width: double.infinity,
+            padding: const EdgeInsets.only(left: 24, right: 24),
+            child: ElevatedButton(
+              onPressed: () {
+                Get.to(Signup());
+              },
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(vertical: 15),
+                backgroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+              child: Text("Sign in", style:  GoogleFonts.openSans(
+                  color: kPrimaryColor,
+                  fontWeight: FontWeight.w700,
+                  fontSize: 18,
+                  height: 1.5,
+                  letterSpacing: .3)),
+            ),),
+    ],);
+  }
+}
+ 
